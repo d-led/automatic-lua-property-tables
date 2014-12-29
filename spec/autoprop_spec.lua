f5e4daa408020581e5621d95e937a9da63c2bba8
Dmitry Ledentsov <dmlled@yahoo.com>
2014-12-29 08:44:44 +0100

    creating new tables

--- spec/autoprop_spec.lua
+++ spec/autoprop_spec.lua
@@ -14,4 +14,11 @@ describe("a utility for creating property paths automatically",function()
 		assert.True(type(autoprop.create()) == 'table')
 		assert.are_equal(0,#autoprop.create())
 	end)
+
+	it("should be able to create properties automatically",function ( )
+		local t = autoprop.create()
+		assert.has_no.errors(function()
+			t.first_level_fresh.second_level_value = {}--42 
+		end)
+	end)
 end)
