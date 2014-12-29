cd81915ac6fbab566feb2051a6dd996847143fb5
Dmitry Ledentsov <dmlled@yahoo.com>
2014-12-29 08:29:50 +0100

    should create an empty table

--- autoprop.lua
+++ autoprop.lua
@@ -1,3 +1,13 @@
 local autoprop = {}
 
+autoprop.create = function()
+	local function get_metatable()
+		local meta = {}
+		return meta
+	end
+
+	local result = {}
+	return result
+end
+
 return autoprop
