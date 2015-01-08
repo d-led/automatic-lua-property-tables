7ae6a9b06bfdaf2e905c63db97605c0326a90176
Dmitry Ledentsov <dmlled@yahoo.com>
2015-01-08 13:56:55 +0100

    refactored

--- autoprop.lua
+++ autoprop.lua
@@ -9,41 +9,41 @@ autoprop.create = function(tracer)
 		end
 	end
 
-	------------
+	local function set_metatable_of_all_values(t)
+		local seen = {}
+		local stack = { t }
+		while #stack > 0 do
+			local candidate = stack[#stack]
+			stack[#stack] = nil
+			if not seen[candidate] and type(candidate)==type{} then
+				setmetatable(candidate,meta)
+				seen[#seen+1] = candidate
+
+				for _,v in pairs(candidate) do
+					stack[#stack+1] = v
+				end
+			end
+		end
+	end
+
+	------------ on access: t.unknown_key
 	meta.__index = function ( table, key )
 		local value = rawget(table, key)
+
 		if value == nil then
+			-- create a new table entry
 			local res = {}
 			setmetatable(res,meta)
 			trace('__index',table,key,res)
 			rawset(table,key,res)
 			return res
 		end
-		if type(value) == 'table' then
-			setmetatable(value,meta)
-		end
 
 		return value
 	end
 
-	---------------
+	--------------- on assignment: t.some_key = value
 	meta.__newindex = function ( table, key, value )
-		local function set_metatable_of_all_values(t)
-			local seen = {}
-			local stack = { t }
-			while #stack > 0 do
-				local candidate = stack[#stack]
-				stack[#stack] = nil
-				if not seen[candidate] and type(candidate)==type{} then
-					setmetatable(candidate,meta)
-					seen[#seen+1] = candidate
-
-					for _,v in pairs(candidate) do
-						stack[#stack+1] = v
-					end
-				end
-			end
-		end
 
 		if type(value) == 'table' then
 			set_metatable_of_all_values(value)
